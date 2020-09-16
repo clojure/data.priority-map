db993170177c73e4cb9ee1a071576a4558894b86
Alex Miller <alex.miller@cognitect.com>
2020-09-16 12:38:38 -0500

    add copyright statement to code

--- src/main/clojure/clojure/data/priority_map.clj
+++ src/main/clojure/clojure/data/priority_map.clj
@@ -1,3 +1,11 @@
+;;   Copyright (c) Mark Engelberg, Rich Hickey and contributors. All rights reserved.
+;;   The use and distribution terms for this software are covered by the
+;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;;   which can be found in the file epl-v10.html at the root of this distribution.
+;;   By using this software in any fashion, you are agreeing to be bound by
+;;   the terms of this license.
+;;   You must not remove this notice, or any other, from this software.
+
 ;; A priority map is a map from items to priorities,
 ;; offering queue-like peek/pop as well as the map-like ability to
 ;; easily reassign priorities and other conveniences.
